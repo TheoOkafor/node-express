c0aa871fa4551874d3ee76f2b64e626d09c228e1
Theo Okafor <theo.okafor@yahoo.com>
2021-12-14 06:37:36 +0100

    feat(proxy): pipe the images through the server

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const routes = express.Router();
+
+let url = 'http://pets-images.dev-apis.com/pets';
+routes.get('/', (req, res) => {
+  const { id } = req.params;
+
+  return fetch(`${url}/dog25.jpg`).then((response) =>
+    response.body.pipe(res)
+  );
+});
+
+routes.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  return fetch(`${url}/${id || 'dog25.jpg'}`).then((response) =>
+    response.body.pipe(res)
+  );
+});
+
+module.exports = routes;
